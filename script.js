e36a0ed88942d2bc85e6b3923a62032c7567b723
Pankaj Mokashi <127533657+pankajmokashi@users.noreply.github.com>
2023-04-25 19:55:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+let numbers = document.querySelectorAll("input")
+let button = document.querySelector("button")
+let ans = document.getElementById("sum")
+
+function add(){
+	let sum = 0
+	sum = numbers[0].value + "+" + numbers[1].value
+	ans.innerText = ""
+	ans.innerHTML = eval(sum)
+}
+button.addEventListener("click", add)
